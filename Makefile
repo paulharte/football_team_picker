54ba3fd20adf6571275bc0771cba11547c67aeb4
Paul Harte <paul.harte@qualio.com>
2025-09-02 09:11:15 +0100

    Updating requirements and adding mac shell script

---
+++ Makefile
@@ -0,0 +1,8 @@
+update_requirements:
+	pip freeze > requirements.txt
+
+start:
+	jupyter notebook
+
+install:
+	./run_notebook_mac.sh
---
+++ run_notebook_mac.sh
@@ -0,0 +1,5 @@
+python3 -m venv ./.venv
+source .venv/bin/activate
+pip3 install -r requirements.txt
+echo "Starting notebook"
+jupyter notebook
